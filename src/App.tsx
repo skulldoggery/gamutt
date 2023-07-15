6dd662e46dae60f0ab39f4c7b43ed63b280f7166
xolotl <xolotl@skulldoggerous.dev>
2023-07-14 23:13:05 -0400

    Better linting, added tailwindcss support

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import type { Component } from 'solid-js';
 const App: Component = () => {
 	return (
 
-		<main class="min-h-screen bg-black py-6 max-w-screen flex justify-center flex-col">
-			<h1 class="text-6xl text-cyan-700 text-center font-mono font-extrabold tracking-widest text-opacity-50">GAMUTT</h1>	
+		<main class="max-w-screen flex min-h-screen flex-col justify-center bg-black py-6">
+			<h1 class="font-mono text-center text-6xl font-extrabold tracking-widest text-cyan-700 text-opacity-50">GAMUTT</h1>	
 			<div class="loader mx-18 my-12">
 				<div class="loader-span" />
 				<div class="loader-blip" />
